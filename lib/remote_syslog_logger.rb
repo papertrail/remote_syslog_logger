506bba071f09082106aacd6d3cc3e17423d99dac
Mark Dascher <mark@papertrail.com>
2018-10-10 12:25:38 -0700

    Release 1.0.4

--- lib/remote_syslog_logger.rb
+++ lib/remote_syslog_logger.rb
@@ -3,7 +3,7 @@ require 'remote_syslog_logger/udp_sender'
 require 'logger'
 
 module RemoteSyslogLogger
-  VERSION = '1.0.3'
+  VERSION = '1.0.4'
 
   def self.new(remote_hostname, remote_port, options = {})
     Logger.new(RemoteSyslogLogger::UdpSender.new(remote_hostname, remote_port, options))
